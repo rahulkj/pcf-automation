cea3d8d18cf0c2831cd7a60fb19d4e32de36c4e0
Rahul Jain <rahulkj@gmail.com>
2019-01-03 16:14:20 -0600

    Adding tasks to inject winfs

---
+++ tasks/inject-winfs/task.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $DEBUG == true ]]; then
+  set -ex
+else
+  set -e
+fi
+
+REPLICATOR_CLI=$(find winfs-injector/ -name "*linux")
+chmod +x $REPLICATOR_CLI
+CMD=./$REPLICATOR_CLI
+
+INPUT_FILE=$(find pivnet-product/ -name "*.pivotal")
+FILE_NAME=$(echo $INPUT_FILE | cut -d '/' -f2)
+OUTPUT_FILE=output-folder/$FILE_NAME
+
+$CMD -name $REPLICATED_NAME -path $INPUT_FILE -output $OUTPUT_FILE
+
+$CMD --input-tile $INPUT_FILE --output-tile $OUTPUT_FILE
+
+cp pivnet-product/metadata.* output-folder/
+cp pivnet-product/version output-folder/

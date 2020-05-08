2a26c1925ee08e5803a80b96d52cc5d10d2d185c
Rahul Jain <rahulkj@gmail.com>
2020-05-08 09:26:40 -0500

    Adding ENV to ytt

--- generate/create_pipeline.sh
+++ generate/create_pipeline.sh
@@ -15,6 +15,9 @@ done
 
 read -p "Enter the pipelines directory: " PIPELINE_DIR
 
+read -p "Enter env name: " ENV
+export YTT_env=${ENV}
+
 folders=(pipelines pipelines/download-products pipelines/ops-manager pipelines/products pipelines/repave)
 
 for folder in "${folders[@]}"; do
@@ -25,32 +28,32 @@ done
 set -e
 
 ytt -f "${BASE_DIR}/download-products/template.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/download-products/pipeline.yml"
-ytt -f "${BASE_DIR}/download-products/template-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/download-products/params-template.yml"
+ytt -f "${BASE_DIR}/download-products/template-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments --data-values-env YTT > "${PIPELINE_DIR}/pipelines/download-products/params-template.yml"
 
 ytt -f "${BASE_DIR}/opsman/template.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/ops-manager/pipeline.yml"
-cp "${BASE_DIR}/opsman/template-params.yml" "${PIPELINE_DIR}/pipelines/ops-manager/params-template.yml"
+ytt -f "${BASE_DIR}/opsman/template-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments --data-values-env YTT > "${PIPELINE_DIR}/pipelines/ops-manager/params-template.yml"
 
-ytt -f "${BASE_DIR}/repave-platform/template.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/repave/pipeline.yml"
-cp "${BASE_DIR}/repave-platform/template-params.yml" "${PIPELINE_DIR}/pipelines/repave/params-template.yml"
+ytt -f "${BASE_DIR}/repave-platform/template.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments --data-values-env YTT > "${PIPELINE_DIR}/pipelines/repave/pipeline.yml"
+ytt -f "${BASE_DIR}/repave-platform/template-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments --data-values-env YTT > "${PIPELINE_DIR}/pipelines/repave/params-template.yml"
 
 ytt -f "${BASE_DIR}/install-upgrade-products/template.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/products/pipeline.yml"
-ytt -f "${BASE_DIR}/install-upgrade-products/template-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/products/params-template.yml"
+ytt -f "${BASE_DIR}/install-upgrade-products/template-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments --data-values-env YTT > "${PIPELINE_DIR}/pipelines/products/params-template.yml"
 
-ytt -f "${BASE_DIR}/globals-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments > "${PIPELINE_DIR}/pipelines/globals.yml"
+ytt -f "${BASE_DIR}/globals-params.yml" -f "${BASE_DIR}/values.yml" --ignore-unknown-comments --data-values-env YTT > "${PIPELINE_DIR}/pipelines/globals.yml"
 
 PRODUCTS=$(yq r "${BASE_DIR}/values.yml" products -j | jq -r '.[] | select(.deploy_product==true) | .name')
 
 for p in ${PRODUCTS}; do
   echo "------ ${p} ------"
-  if [[ ! -d "${PIPELINE_DIR}/config/${p}" ]]; then
-    mkdir -p "${PIPELINE_DIR}/config/${p}" && touch "${PIPELINE_DIR}/config/${p}/.gitkeep"
+  if [[ ! -d "${PIPELINE_DIR}/${ENV}/config/${p}" ]]; then
+    mkdir -p "${PIPELINE_DIR}/${ENV}/config/${p}" && touch "${PIPELINE_DIR}/${ENV}/config/${p}/.gitkeep"
   fi
 
-  if [[ ! -d "${PIPELINE_DIR}/vars/${p}" ]]; then
-    mkdir -p "${PIPELINE_DIR}/vars/${p}" && touch "${PIPELINE_DIR}/vars/${p}/.gitkeep"
+  if [[ ! -d "${PIPELINE_DIR}/${ENV}/vars/${p}" ]]; then
+    mkdir -p "${PIPELINE_DIR}/${ENV}/vars/${p}" && touch "${PIPELINE_DIR}/${ENV}/vars/${p}/.gitkeep"
   fi
 
-  if [[ ! -d "${PIPELINE_DIR}/env" ]]; then
-    mkdir -p "${PIPELINE_DIR}/env" && touch "${PIPELINE_DIR}/env/.gitkeep"
+  if [[ ! -d "${PIPELINE_DIR}/${ENV}/env" ]]; then
+    mkdir -p "${PIPELINE_DIR}/${ENV}/env" && touch "${PIPELINE_DIR}/${ENV}/env/.gitkeep"
   fi
 done;
